f4121fc8df6b4e4a39b26be3a152daeeccef38bd
ayaovi <djayespoiro@gmail.com>
2017-05-12 18:59:50 +0200

    feat: Build city network
    Built city network and got shortest path algorithm working

--- Dijkstra/Edge.cs
+++ Dijkstra/Edge.cs
@@ -4,15 +4,17 @@ namespace Dijkstra
 {
   class Edge
   {
-    public Edge()
+    public Edge(string id, Vertex source, Vertex destination)
     {
+      Id = id;
+      Source = source;
+      Destination = destination;
       Weight = (int)Math.Sqrt((Destination.X - Source.X) * (Destination.X - Source.X) + (Destination.Y - Source.Y) * (Destination.Y - Source.Y));
     }
 
     public string Id { get; set; }
     public Vertex Source { get; set; }
     public Vertex Destination { get; set; }
-
     public int Weight { get; set; }
 
     public override string ToString()
--- Dijkstra/Program.cs
+++ Dijkstra/Program.cs
@@ -17,78 +17,41 @@ namespace Dijkstra
         new[]{ 338, 210 }, new[]{ 338, 310 }, new[]{ 236, 210 }, new[]{ 236, 360 },
         new[]{ 445, 260 }, new[]{ 600, 260 }, new[]{ 480, 400 }, new[]{ 338, 400 },
         new[]{ 236, 400 }, new[]{ 236, 260 }, new[]{ 87, 260 }, new[]{ 110, 400 },
-        new[]{ 0, 305 }, new[]{ 87, 200 }, new[]{ 0, 200 }, new[]{ 445, 200 },
-
+        new[]{ 0, 305 }, new[]{ 87, 200 }, new[]{ 0, 200 }, new[]{ 445, 200 }
       };
-      var vertices = Enumerable.Range(0, 6)
+
+      var verticesConnection = new[]
+      {
+        new[]{0, 1}, new[]{1, 0}, new[]{1, 2}, new[]{2, 1}, new[]{1, 29}, new[]{29, 1}, 
+        new[]{2, 3}, new[]{3, 2}, new[]{3, 4}, new[]{4, 3}, new[]{3, 5}, new[]{5, 3}, 
+        new[]{5, 6}, new[]{6, 5}, new[]{5, 7}, new[]{7, 5}, new[]{5, 18}, new[]{18, 5}, 
+        new[]{7, 8}, new[]{8, 7}, new[]{7, 9}, new[]{9, 7}, new[]{9, 10}, new[]{10, 9}, 
+        new[]{9, 11}, new[]{11, 9}, new[]{10, 12}, new[]{12, 10}, new[]{10, 31}, new[]{31, 10}, 
+        new[]{12, 13}, new[]{13, 12}, new[]{13, 14}, new[]{14, 13}, new[]{13, 15}, new[]{15, 13}, 
+        new[]{11, 31}, new[]{31, 11}, new[]{11, 16}, new[]{16, 11}, new[]{31, 20}, new[]{20, 31}, 
+        new[]{20, 21}, new[]{21, 20}, new[]{20, 17}, new[]{17, 20}, new[]{20, 22}, new[]{22, 20}, 
+        new[]{16, 17}, new[]{17, 16}, new[]{16, 18}, new[]{18, 16}, new[]{17, 23}, new[]{23, 17}, 
+        new[]{17, 19}, new[]{19, 17}, new[]{19, 24}, new[]{24, 19}, new[]{19, 25}, new[]{25, 19}, 
+        new[]{18, 25}, new[]{25, 18}, new[]{25, 26}, new[]{26, 25}, new[]{26, 27}, new[]{27, 26}, 
+        new[]{26, 28}, new[]{28, 26}, new[]{26, 29}, new[]{29, 26}, new[]{29, 30}, new[]{30, 29}
+      };
+
+      var vertices = Enumerable.Range(0, verticesPosition.Length)
                                .Select(i => new Vertex
                                {
                                  Id = "Node_" + i,
-                                 Name = "Node_" + i
+                                 Name = "Node_" + i,
+                                 X = verticesPosition[i][0],
+                                 Y = verticesPosition[i][1]
                                })
                                .ToList();
 
-      var edges = new List<Edge>
-      {
-        new Edge
-        {
-          Id = "Edge_0",
-          Source = vertices[0],
-          Destination = vertices[1]
-        },
-        new Edge
-        {
-          Id = "Edge_1",
-          Source = vertices[0],
-          Destination = vertices[2]
-        },
-        new Edge
-        {
-          Id = "Edge_2",
-          Source = vertices[0],
-          Destination = vertices[3]
-        },
-        new Edge
-        {
-          Id = "Edge_3",
-          Source = vertices[1],
-          Destination = vertices[4]
-        },
-        new Edge
-        {
-          Id = "Edge_4",
-          Source = vertices[2],
-          Destination = vertices[4]
-        },
-        new Edge
-        {
-          Id = "Edge_4",
-          Source = vertices[2],
-          Destination = vertices[5]
-        },
-        new Edge
-        {
-          Id = "Edge_4",
-          Source = vertices[2],
-          Destination = vertices[3]
-        },
-        new Edge
-        {
-          Id = "Edge_4",
-          Source = vertices[3],
-          Destination = vertices[5]
-        },
-        new Edge
-        {
-          Id = "Edge_4",
-          Source = vertices[4],
-          Destination = vertices[5]
-        }
-      };
+      var edges = Enumerable.Range(0, verticesConnection.Length)
+                               .Select(i => new Edge("Edge_" + i,vertices[verticesConnection[i][0]],vertices[verticesConnection[i][1]]))
+                               .ToList();
 
       var dijkstra = new Dijkstra(vertices, edges);
-      var shortesPath = dijkstra.FindShortestPath(vertices[0], vertices[5]);
-      Console.WriteLine(shortesPath.Count);
+      var shortesPath = dijkstra.FindShortestPath(vertices[31], vertices[26]);
 
       foreach (Vertex vertex in shortesPath)
       {
